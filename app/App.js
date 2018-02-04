a6d1ad425e3f8350192ca592329e514bc0d30f9c
asddsajpg <asddsajpg@mgail.com>
2018-02-05 00:05:05 +0400

    created local notification

--- app/App.js
+++ app/App.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
 import { Platform, StyleSheet } from 'react-native';
 import firebase from 'react-native-firebase';
+import PushNotification from 'react-native-push-notification';
 
 import MainStack from './Navigator';
 
-firebase.messaging().onMessage((obj) => {
-	Alert.alert(JSON.stringify(obj));
+firebase.messaging().onMessage((msg) => {
+	let { title, body } = msg.fcm;
+	PushNotification.localNotification({
+		title: title,
+		message: body
+	});
 });
 
 export default class App extends Component<{}> {
