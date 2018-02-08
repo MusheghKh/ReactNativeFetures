2d258f4ac9c2b31e8fe637653c0c87d341ebfed0
asddsajpg <asddsajpg@mgail.com>
2018-02-08 22:05:23 +0400

    run this script when wachman doestn't want work

---
+++ wachmanshucks.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Run this if wachman start sucking
+
+echo fs.inotify.max_user_instances=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+echo fs.inotify.max_queued_events=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
